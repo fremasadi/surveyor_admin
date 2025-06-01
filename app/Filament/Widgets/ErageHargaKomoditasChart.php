30809dd7b2e760131004aeebf17bc755fd861786
fremasadi <fremasadii@gmail.com>
2025-06-01 22:00:36 +0700

    feat

--- app/Filament/Widgets/ErageHargaKomoditasChart.php
+++ app/Filament/Widgets/ErageHargaKomoditasChart.php
@@ -5,24 +5,48 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\ChartWidget;
 use App\Models\DataHarian;
 use App\Models\Komoditas;
+use Filament\Forms\Components\Select;
 
 class ErageHargaKomoditasChart extends ChartWidget
 {
     protected static ?string $heading = 'Rata-rata Harga per Komoditas';
+    
+    public ?string $filter = null;
+
+    protected function getFilters(): ?array
+    {
+        $komoditasOptions = Komoditas::pluck('name', 'id')->toArray();
+        
+        return [
+            null => 'Semua Komoditas',
+            ...$komoditasOptions
+        ];
+    }
 
     protected function getData(): array
     {
         $labels = [];
         $data = [];
 
-        $komoditasList = Komoditas::all();
+        // Query builder untuk komoditas dengan filter
+        $komoditasQuery = Komoditas::query();
+        
+        // Jika ada filter yang dipilih, filter berdasarkan ID
+        if ($this->filter) {
+            $komoditasQuery->where('id', $this->filter);
+        }
+        
+        $komoditasList = $komoditasQuery->get();
 
         foreach ($komoditasList as $komoditas) {
             $avg = DataHarian::where('komoditas_id', $komoditas->id)
                 ->avg('data_input');
 
-            $labels[] = $komoditas->name;
-            $data[] = round($avg, 2);
+            // Hanya tampilkan jika ada data
+            if ($avg !== null) {
+                $labels[] = $komoditas->name;
+                $data[] = round($avg, 2);
+            }
         }
 
         return [
